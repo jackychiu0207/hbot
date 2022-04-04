486b0f31592b067c453e12056cd29ff96621e202
jacky <66721680+jackychiu0207@users.noreply.github.com>
2022-04-05 02:11:10 +0800

    update main.py

--- main.py
+++ main.py
@@ -116,7 +116,7 @@ def embed_n(data:dict,lang):
     text=""
     if 'text' in data:text+=change_text(data["text"][lang])+"\n\n"
     if 'flavor' in data:text+=change_text(data['flavor'][lang])
-    imgurl=f"https://art.hearthstonejson.com/v1/render/latest/{lang}/512x/"+data["id"]+".png"
+    imgurl=f"https://art.hearthstonejson.com/v1/render/latest/{lang}/orig/"+data["id"]+".png"
     cardview=f"https://playhearthstone.com/cards/"+str(data["dbfId"])
     if requests.request('GET',imgurl).status_code==404:
         imgurl=f"https://art.hearthstonejson.com/v1/256x/"+data["id"]+".jpg"
@@ -141,7 +141,7 @@ def embed_bg(data:dict,lang):
     view=View()
     title=data['name'][lang]
     text=""
-    imgurl=f"https://art.hearthstonejson.com/v1/render/latest/{lang}/512x/"+data["id"]+".png"
+    imgurl=f"https://art.hearthstonejson.com/v1/render/latest/{lang}/orig/"+data["id"]+".png"
     cardview=f"https://playhearthstone.com/battlegrounds/"+str(data["dbfId"])
     token=get_token()
     bzlang=lang[0:2]+"_"+lang[2:4]
@@ -197,7 +197,7 @@ def embed_m(data:dict,lang):
     view=View()
     title=data['name'][lang]
     text=""
-    imgurl=f"https://art.hearthstonejson.com/v1/render/latest/{lang}/512x/"+data["id"]+".png"
+    imgurl=f"https://art.hearthstonejson.com/v1/render/latest/{lang}/orig/"+data["id"]+".png"
     cardview=f"https://playhearthstone.com/zh-tw/mercenaries/"+str(data["dbfId"])
     if requests.request('GET',imgurl).status_code==404:
         imgurl=f"https://art.hearthstonejson.com/v1/256x/"+data["id"]+".jpg"
@@ -304,8 +304,8 @@ def embed_m(data:dict,lang):
                                                         if tiers["dbf_id"] == c_data["dbfId"]:
                                                              if tiers['tier']==1:text+=f"技能{str(i)}等級1:{c_data['name'][lang]}({c_data['dbfId']})\n"
                                                              d=change_text(c_data['text'][lang]).replace('*','').replace('\n','')
-                                                             if len(d)>80:d=d[0:75]+"..."
-                                                             options_p.append(SelectOption(label=f"{c_data['name'][lang]}(技能{str(i)}等級{tiers['tier']})\n",value=str(c_data['dbfId']),description=d))
+                                             if len(d)>80:d=d[0:75]+"..."
+                                             options_p.append(SelectOption(label=f"{c_data['name'][lang]}(技能{str(i)}等級{tiers['tier']})\n",value=str(c_data['dbfId']),description=d))
                                         select_p=Select(placeholder="選擇要查看的技能",options=options_p,min_values=1,max_values=1)
                                         select_p.callback=select_new_embed
                                         view.add_item(select_p)
