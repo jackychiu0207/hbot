5e07470c754298066b3f45550f604119027aa109
jacky <66721680+jackychiu0207@users.noreply.github.com>
2022-04-05 01:56:56 +0800

    update main.py

--- main.py
+++ main.py
@@ -287,7 +287,7 @@ def embed_m(data:dict,lang):
                                         if tiers['tier']==1:text+=f"裝備{str(i)}等級1:{c_data['name'][lang]}({c_data['dbfId']})\n"
                                         d=change_text(c_data['text'][lang]).replace('*','').replace('\n','')
                                         if len(d)>80:d=d[0:75]+"..."
-                                        options_e.append(SelectOption(label=f"{c_data['name'][lang]}(裝備{str(i)}等級{tiers['tier']})\n",value=str(c_data['dbfId']),description=d')
+                                        options_e.append(SelectOption(label=f"{c_data['name'][lang]}(裝備{str(i)}等級{tiers['tier']})\n",value=str(c_data['dbfId']),description=d))
                         select_e=Select(placeholder="選擇要查看的裝備",options=options_e,min_values=1,max_values=1)
                         select_e.callback=select_new_embed
                         view.add_item(select_e)
@@ -303,7 +303,7 @@ def embed_m(data:dict,lang):
                                                     for c_data in cardlib:
                                                         if tiers["dbf_id"] == c_data["dbfId"]:
                                                              if tiers['tier']==1:text+=f"技能{str(i)}等級1:{c_data['name'][lang]}({c_data['dbfId']})\n"
-                                             d=change_text(c_data['text'][lang]).replace('*','').replace('\n','')
+                                             d=change_text(c_data['text'][lang]).replace('*','').replace('\n',''))
                                              if len(d)>80:d=d[0:75]+"..."
                                              options_p.append(SelectOption(label=f"{c_data['name'][lang]}(技能{str(i)}等級{tiers['tier']})\n",value=str(c_data['dbfId']),description=d))
                                         select_p=Select(placeholder="選擇要查看的技能",options=options_p,min_values=1,max_values=1)
