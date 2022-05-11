3d71c137f4b2c265bbc3fc04516fd4777ebc066e
jackychiu0207 <jackychiu0101@gmail.com>
2022-05-11 17:44:15 +0800

    remove update

--- update.py
+++
@@ -1,9 +0,0 @@
-import os
-import wget
-try:
-    os.remove('main.py')
-    os.remove('requirements.txt')
-except:pass
-wget.download('https://raw.githubusercontent.com/jackychiu0207/hbot/main/main.py',out='main.py')
-wget.download('https://raw.githubusercontent.com/jackychiu0207/hbot/main/requirements.txt',out='requirements.txt')
-os.system('pip install -r requirements.txt')
